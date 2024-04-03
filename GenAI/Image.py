299179c51a76b26d908bf4bf0fe37b4276a25ff8
shashisingh612 <shashi.singh91@outlook.com>
2024-04-03 17:13:28 +0530

    Your commit message here

---
+++ GenAI/Image.py
@@ -0,0 +1,34 @@
+import os
+import urllib.request
+from openai import OpenAI
+api_key="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"
+client = OpenAI(api_key=api_key)
+#client = OpenAI()
+
+
+newImage = client.images.generate(
+  prompt="A UFO abducting a chicken",
+  n=2,
+  size="1024x1024"
+)
+
+print("Here are two generated images of a UFO abducting a chicken:\n")
+
+print(newImage.data[0].url)
+
+print("\n")
+
+print(newImage.data[1].url)
+
+# Download the first one
+urllib.request.urlretrieve(newImage.data[0].url, "chicken.png")
+
+# Ask for a variation
+newImage = client.images.create_variation(
+    image = open("chicken.png", "rb"),
+    n=1,
+    size="1024x1024"
+)
+
+print("\nHere's a variation of the first image:\n")
+print(newImage.data[0].url)
